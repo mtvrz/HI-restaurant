c4979a0f8422c6fc6cd4eadba8238d1c00a21f73
mtvrz <89358718+mtvrz@users.noreply.github.com>
2024-02-11 11:33:32 +0100

    main

---
+++ src/components/MainJS.js
@@ -0,0 +1,13 @@
+import classes from "./MainJS.module.css";
+import Login from "./login/Login";
+import Slideshow from "./slideShow/Slideshow";
+import Dashboard from "./dashboard/Dashboard";
+
+const MainJS = () =>{
+    //tests
+    return <div>
+        <Slideshow/>
+    </div>
+}
+
+export default MainJS
